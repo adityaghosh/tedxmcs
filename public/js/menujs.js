c2a8a59c784cbf2288dadf199ea0d86d834c7ede
Aditya Ghosh <aditya_bhaiti@yahoo.co.in>
2016-02-11 16:25:26 -0500

    Adding project files

---
+++ public/js/menujs.js
@@ -0,0 +1,67 @@
+
+	//menubar item 1
+	function changeImageTed2()
+	{
+		
+		document.getElementById('ted1').src="images/menu/1 ted1.png";
+		
+	}
+	function changeImageTed()
+	{
+		document.getElementById('ted1').src="images/menu/1 ted2.png";
+		}
+	//menu bar item 2
+	function changeImageSpeakers2()
+	{
+		
+		document.getElementById('ted2').src="images/menu/2 speakers1.png";
+		
+	}
+	function changeImageSpeakers()
+	{
+		document.getElementById('ted2').src="images/menu/2 speakers2.png";
+		}
+	//menubar item 3
+	function changeImageTeam2()
+	{
+		
+		document.getElementById('ted3').src="images/menu/3 team1.png";
+		
+	}
+	function changeImageTeam()
+	{
+		document.getElementById('ted3').src="images/menu/3 team2.png";
+		}
+	//menubar item 4
+	function changeImageEvent2()
+	{
+		
+		document.getElementById('ted4').src="images/menu/4 events1.png";
+		
+	}
+	function changeImageEvent()
+	{
+		document.getElementById('ted4').src="images/menu/4 events2.png";
+		}
+	//menubar item 5
+	function changeImageSponsors2()
+	{
+		
+		document.getElementById('ted5').src="images/menu/5 sponsors1.png";
+		
+	}
+	function changeImageSponsors()
+	{
+		document.getElementById('ted5').src="images/menu/5 sponsors2.png";
+		}
+	//menubar item 6
+	function changeImageAbout2()
+	{
+		
+		document.getElementById('ted6').src="images/menu/6 about1.png";
+		
+	}
+	function changeImageAbout()
+	{
+		document.getElementById('ted6').src="images/menu/6 about2.png";
+		}
